03cdec5bef02302f933b732b8dc13a2c694e0333
rafael-dscarvalho <rafaeldscarvalho@hotmail.com>
2023-11-19 18:27:22 -0300

    arrumei oq faltava

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ export  function Home(){
       <main>
         <div id="DivInterna">
           <img id="Imagem" src="img1.jpg"/>
-          <h1>O seu próximo <span id="Destino">destino</span> pode estar aqui...</h1>
+          <h1>O seu próximo <strong className="destino">destino</strong> pode estar aqui...</h1>
           <p className="p">Aqui você encontra uma seleção de <span className="Destaque">pacotes completos</span> para as suas férias!
           </p>
           <p className="p" id="Espacamento">No <span id="Opacidade">ConnectFlight</span> você encontra pacotes de viagens nacionais e internacionais que incluem: passagens aéreas, hotéis, café da manhã, 
@@ -18,9 +18,11 @@ export  function Home(){
         </div>
       </main>
       <footer>
-        <label className="Labels">Destinos</label>
-        <label id="Oferta" className="Labels">Ofertas</label>
-        <label className="Labels">Contato</label>
+        <ul>
+          <li><strong>Destinos</strong></li>
+          <li><strong>Ofertas</strong></li>
+          <li><strong>Contato</strong></li>
+        </ul>
       </footer>
     </>
     
